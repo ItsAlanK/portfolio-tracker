749cf63dac5b7a3e082dced20e85dae465bdeb47
Alan Kinsella <alankinsella1993@gmail.com>
2021-09-15 19:50:30 +0000

    Fixed linting errors

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 import finnhub
 import finnhubkey
-from helpers import *
+from helpers import validate
+import time
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,14 +20,16 @@ FINNHUB_CLIENT = finnhub.Client(api_key=finnhubkey.key)
 
 def start_program():
     """
-    Provide opening query to user
+    Provide opening option to user
     """
     while True:
-        expected_responses = ["L","P"]
-        print(f"If you would like to check Live Stock or Crypto prices press {expected_responses[0]}.")
-        print(f"If you would like to View or Edit your personal positions press {expected_responses[1]}.")
-        response = input(f"Enter your response ({expected_responses[0]}/{expected_responses[1]}):\n")
-        
+        expected_responses = ["L", "P"]
+        print(f"If you would like to check Live Stock or Crypto prices press \
+            {expected_responses[0]}.")
+        print(f"If you would like to View or Edit your personal positions press \
+            {expected_responses[1]}.")
+        response = input(f"Enter your response \
+            ({expected_responses[0]}/{expected_responses[1]}):\n")
         if validate(response, expected_responses):
             break
 
@@ -39,4 +42,6 @@ def main():
 
 
 print("Welcome to your own personal Portfolio Tracker!\n")
-main()
+time = int(time.time())
+timelast = time - 60
+print(FINNHUB_CLIENT.crypto_candles('BINANCE:DOGEUSDT', '1', timelast, time))
