6bb1d0aec0f7516cb2c77ba48f313cfe062c4c70
Alan Kinsella <alankinsella1993@gmail.com>
2021-09-16 15:37:46 +0000

    Make get_live_data function parse live data recieved and return only current price

--- srv/requests.py
+++ srv/requests.py
@@ -5,8 +5,10 @@ FINNHUB_CLIENT = finnhub.Client(api_key=env.FINNHUB_KEY)
 
 
 def get_live_data(ticker):
-    print(FINNHUB_CLIENT.quote(ticker))
-    print(ticker)
+    raw_data = FINNHUB_CLIENT.quote(ticker)
+    current_price_raw = raw_data["c"]
+    current_price = round(current_price_raw, 2)
+    return current_price
 
 
 def get_all_symbols():
